415f64efc402574635229ae721ba9ddd5ef685e2
piyush9125 <pdpatil9125@gmail.com>
2025-02-25 20:47:51 +0530

    changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AGPractice';
+  hh='piyush';
+
+  students=[{name:'',age:''}];
+  
+  add(std:any){
+    
+    console.log(std)
+    
+  
+  }
+
+
 }
