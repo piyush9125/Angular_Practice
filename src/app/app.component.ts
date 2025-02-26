874adb469f45254ec02978ada02dcf522e82c253
piyush9125 <pdpatil9125@gmail.com>
2025-02-27 01:39:04 +0530

    changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AGPractice';
-  hh='piyush';
+student=[
+  {name:'kapil',age:12,isEdit:false,buttoname:'edit'},
+  {name:'jayesh',age:13,isEdit:false,buttoname:'edit'},
+  {name:'piyush',age:10,isEdit:false,buttoname:'edit'}
+]
 
-  students=[{name:'',age:''}];
-  
-  add(std:any){
-    
-    console.log(std)
-    
-  
+button='show';
+changed(std:any){
+
+  if(std.isEdit===false&&std.buttoname==="edit"){
+    std.isEdit=true;
+    std.buttoname='add';
+
+  }else{
+    std.isEdit=false;
+    std.buttoname='edit'
   }
-
+  console.log(std);
+}
 
 }
